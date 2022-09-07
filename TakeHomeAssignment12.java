88c81d78a050f58edc35c73e8cd9d745ceffa71c
ksschemmel <112411813+ksschemmel@users.noreply.github.com>
2022-09-07 11:53:07 -0400

    Add files via upload

---
+++ TakeHomeAssignment12.java
@@ -0,0 +1,11 @@
+public class TakeHomeAssignment12
+{
+public static void main(String args[])
+{
+Double a = Double.parseDouble(args[0]);
+int b = Integer.parseInt(args[1]);
+
+System.out.println("Multiplication:"+(a*b));
+}
+
+}	
