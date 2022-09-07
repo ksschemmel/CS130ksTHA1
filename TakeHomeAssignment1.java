366f3629868c3ecd221473ead15c5ca1c8da906f
ksschemmel <112411813+ksschemmel@users.noreply.github.com>
2022-09-07 11:46:59 -0400

    Add files via upload

---
+++ TakeHomeAssignment1.java
@@ -0,0 +1,11 @@
+public class TakeHomeAssignment1
+{
+public static void main(String args[])
+{
+System.out.println("My first name is: " + args[0]);
+System.out.println("My last name is: " + args[1]);
+System.out.println("My hobby is: " + args[2]);
+System.out.println("My Major is: " + args[3]);
+}
+
+}
